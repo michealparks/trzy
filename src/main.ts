f93c0d19655f673d00a625efec0b4bd3ffe6c1cc
Micheal Parks <michealparks1989@gmail.com>
2023-01-31 15:37:07 -0500

    force install deps

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export { OrbitControlsGizmo } from './gizmo'
 export { Html } from './html'
 export { resizeRendererToDisplaySize } from './lib/render-to-display-size'
 export { runUpdates, update, removeUpdate } from './lib/update'
+
