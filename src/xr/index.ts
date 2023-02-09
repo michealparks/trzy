3feed355af072a8aa8faa65748911c545e2425e1
Micheal Parks <michealparks1989@gmail.com>
2023-02-08 19:35:41 -0500

    export xr object

--- src/xr/index.ts
+++ src/xr/index.ts
@@ -2,47 +2,47 @@ import type { WebGLRenderer } from 'three'
 
 let session: XRSession | undefined
 
-export const xrState = {
+const state = {
   sessionGranted: false,
 }
 
-export const xrSupportStates = {
+const supportStates = {
   ALLOWED: 0,
   NOT_ALLOWED: 1,
   NOT_SECURE: 2,
   NOT_SUPPORTED: 3,
 } as const
 
-export const xrSupportStateMessages = {
+const supportStateMessages = {
   0: 'Enter VR',
   1: 'VR is not allowed',
   2: 'VR requires HTTPS',
   3: 'VR not supported',
 } as const
 
-export const requestXrSessionSupport = async () => {
+const requestSessionSupport = async () => {
   if (navigator.xr === undefined) {
-    return xrSupportStates.NOT_SUPPORTED
+    return supportStates.NOT_SUPPORTED
   }
 
   if (!window.isSecureContext) {
-    return xrSupportStates.NOT_SECURE
+    return supportStates.NOT_SECURE
   }
 
   try {
     const supported = await navigator.xr.isSessionSupported('immersive-vr')
 
     if (supported) {
-      return xrSupportStates.ALLOWED
+      return supportStates.ALLOWED
     }
 
-    return xrSupportStates.NOT_SUPPORTED
+    return supportStates.NOT_SUPPORTED
   } catch {
-    return xrSupportStates.NOT_ALLOWED
+    return supportStates.NOT_ALLOWED
   }
 }
 
-export const requestSession = async (renderer: WebGLRenderer) => {
+const requestSession = async (renderer: WebGLRenderer) => {
   if (navigator.xr === undefined) {
     throw new Error('navigator.xr is undefined!')
   }
@@ -59,21 +59,22 @@ export const requestSession = async (renderer: WebGLRenderer) => {
   return renderer.xr.setSession(session)
 }
 
-export const endSession = () => {
+const endSession = () => {
   if (session === undefined) {
     throw new Error('Tried to end undefined session!')
   }
+
   session.end()
 }
 
-export const createXrButton = async (renderer: WebGLRenderer) => {
-  const xrSupport = await requestXrSessionSupport()
+const createButton = async (renderer: WebGLRenderer) => {
+  const xrSupport = await requestSessionSupport()
   const button = document.createElement('button')
-  button.textContent = xrSupportStateMessages[xrSupport]
+  button.textContent = supportStateMessages[xrSupport]
 
-  if (xrSupport === xrSupportStates.ALLOWED) {
+  if (xrSupport === supportStates.ALLOWED) {
     button.addEventListener('click', () => {
-      renderer.domElement.style.cssText = 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh;'
+      renderer.domElement.style.cssText = 'position:fixed;top:0;left:0;width:100vw;height:100vh;'
       return requestSession(renderer)
     })
   }
@@ -92,6 +93,16 @@ if (
   // Do nothing
 } else {
   navigator.xr.addEventListener('sessiongranted', () => {
-    xrState.sessionGranted = true
+    state.sessionGranted = true
   })
+}
+
+export const xr = {
+  state,
+  supportStates,
+  supportStateMessages,
+  createButton,
+  requestSessionSupport,
+  requestSession,
+  endSession,
 }
