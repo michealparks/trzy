55ae887addf6e8f768fa976f44e06cef8dba975d
michealparks <michealparks1989@gmail.com>
2023-04-24 03:59:06 +0000

    Deploying to gh-pages from @ michealparks/trzy@b80932f1b9efcb0c28d3f5fbeafb2737495f55b7 🚀

---
+++ assets/index.stories-feb7a3e2.js
@@ -0,0 +1,12 @@
+import{t as c,A as l}from"./index-99b0b950.js";import{s as d}from"./lib-54ac10ae.js";const i=`import { AxesHelper } from 'trzy'
+
+const helper = new AxesHelper({ size: 1.5, width: 0.0025 })
+
+scene.add(helper.setColors('yellow', 'blue', 'pink'))
+
+// later
+helper.dispose()
+`,H={title:"Axes Helper",parameters:{docs:{description:{component:"Similar to `THREE.AxesHelper` but uses `THREE.Line2` to render axes."},source:{code:i}}}},m=()=>{const{scene:r,camera:a,canvas:s,update:p}=c();return d({scene:r,camera:a,canvas:s,update:p,controls:!0}),r.add(new l({size:1.5,width:.0025}).setColors("yellow","blue","pink")),s},e={render:m};var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render
+}`,...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const h=["Primary"];export{e as Primary,h as __namedExportsOrder,H as default};
+//# sourceMappingURL=index.stories-feb7a3e2.js.map
---
+++ assets/syntaxhighlighter-QTQ2UBB4-0f851c64.js
@@ -0,0 +1,2 @@
+import{S as l,c as n,s as d}from"./DocsRenderer-7FRJXR4N-0a00f9bb.js";import"./iframe-981a9257.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-e1f3a364.js";import"./index-356e4a49.js";import"./preview-4b2aa57c.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
+//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-0f851c64.js.map
