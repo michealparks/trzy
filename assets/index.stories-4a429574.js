03142d5dad9604f0fb46835076d84e977689c89c
michealparks <michealparks1989@gmail.com>
2023-04-29 18:54:52 +0000

    Deploying to gh-pages from @ michealparks/trzy@6e874bec5900059d8778ae55a852eac5771454b6 🚀

--- assets/index.stories-4a429574.js
+++
@@ -1,7 +0,0 @@
-import{t as d,O as l,A as u}from"./index-0a4f758e.js";import{s as x}from"./lib-b63f08a3.js";const z=`import { OrbitControlsGizmo } from 'trzy'
-
-new OrbitControlsGizmo({ camera, el, controls })
-`,h={title:"Orbit Controls Gizmo",parameters:{docs:{source:{code:z}}}},O=()=>{const{scene:o,camera:s,canvas:n,update:i}=d(),r=document.createElement("container");return x({scene:o,camera:s,canvas:n,update:i,controls:!0}).then(p=>{r.append(n);const t=document.createElement("div");t.style.cssText="position: absolute; top: 50px; right: 40px;",r.append(t),new l({camera:s,el:t,controls:p}),o.add(new u({size:2,width:.002}))}),r},e={render:O};var a,c,m;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render
-}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const w=["Primary"];export{e as Primary,w as __namedExportsOrder,h as default};
-//# sourceMappingURL=index.stories-4a429574.js.map
--- assets/syntaxhighlighter-QTQ2UBB4-79677be6.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./DocsRenderer-7FRJXR4N-c8bf6cba.js";import"./iframe-84981d24.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-e1f3a364.js";import"./index-356e4a49.js";import"./preview-4a08ccb1.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-79677be6.js.map
