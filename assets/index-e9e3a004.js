1c485ce0c9d5d3eb94b402bfa2c6616b35ef8b85
michealparks <michealparks1989@gmail.com>
2023-07-04 05:44:27 +0000

    Deploying to gh-pages from @ michealparks/trzy@adaa31ebc82bdc6ecfcfeb747798084cde9ce105 🚀

--- assets/index-e9e3a004.js
+++
@@ -1,2 +0,0 @@
-import{M,a as D,u as X,w as C}from"./index-c96f79d3.js";import"./index-f43138ce.js";import"./iframe-86f6a011.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-c45868c5.js";import"./index-356e4a49.js";export{M as MDXContext,D as MDXProvider,X as useMDXComponents,C as withMDXComponents};
-//# sourceMappingURL=index-e9e3a004.js.map
---
+++ assets/index.stories-ac1d03f3.js
@@ -0,0 +1,10 @@
+import{s as w,a as m}from"./setup-1360b994.js";const t=new Set;let y=!1,c=!1;const o={keys:{},controlling:!1,shift:0,space:0,r:0,x:0,y:0},s=(r,e)=>{const a=r.toLowerCase();switch(a){case"s":case"arrowdown":o.y=-1*e;break;case"w":case"arrowup":o.y=1*e;break;case"a":case"arrowleft":o.x=-1*e;break;case"d":case"arrowright":o.x=1*e;break;case" ":o.space=1*e;break;case"q":o.r=1*e;break;case"e":o.r=-1*e;break;case"shift":o.shift=1*e;break}e===1?o.keys[a]=!0:delete o.keys[a]},f=r=>{c&&(r.preventDefault(),r.stopImmediatePropagation());const e=r.key.toLowerCase();o.controlling=!0,t.add(e);for(const a of t)s(a,1)},p=r=>{c&&(r.preventDefault(),r.stopImmediatePropagation());const e=r.key.toLowerCase();t.delete(e),s(e,0);for(const a of t)s(a,1);t.size===0&&(o.controlling=!1)},u=()=>{for(const r of t)s(r,0);t.clear()},k=()=>{window.removeEventListener("keydown",f),window.removeEventListener("keyup",p),window.addEventListener("blur",u),y=!1},b=(r={})=>(c=r.preventDefault??!1,y||(window.addEventListener("keydown",f),window.addEventListener("keyup",p),window.addEventListener("blur",u)),{keyboard:o,disposeKeyboard:k}),K=`import { useKeyboard } from 'trzy'
+
+const { keyboard, disposeKeyboard } = useKeyboard({ preventDefault: true })
+
+// later
+disposeKeyboard()
+`,h={title:"Keyboard",parameters:{docs:{source:{code:K}}}},v=()=>{w();const r=document.createElement("container"),e=document.createElement("pre");r.append(e);const{keyboard:a}=b();return m(()=>{e.innerHTML=JSON.stringify(a,void 0,"  ")}),r},n={render:v};var d,i,l;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render
+}`,...(l=(i=n.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const E=["Primary"];export{n as Primary,E as __namedExportsOrder,h as default};
+//# sourceMappingURL=index.stories-ac1d03f3.js.map
